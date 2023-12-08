f26d6d04fbe510a837b89ecec3c3ed62269d9945
Deszher <143352282+Deszher@users.noreply.github.com>
2023-12-09 02:17:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 
 import streamlit as st
-import PyPDF2
+from PyPDF2 import PdfReader
 from docx import Document
 
 from summarizator import Summarizator
@@ -45,7 +45,7 @@ st.sidebar.info(
 # Считываем текст из PDF-файла
 def read_pdf(file_path):
     with open(file_path, "rb") as file:
-        reader = PyPDF2.PdfFileReader(file)
+        reader = PdfReader(file)
         text = ""
         for page in reader.pages:
             text += page.extract_text()
